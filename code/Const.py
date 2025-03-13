d963c62ec6118c9d9fae7f1f6c926add36bfbfde
Silvia Akiba <projetocomeceporaqui@gmail.com>
2025-03-13 20:33:23 -0300

    Creating menu iterations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -3,13 +3,26 @@ C_ORANGE = (255, 120, 0)
 C_WHITE = (255, 255, 255)
 C_YELLOW = (255, 255, 0)
 
+# E
+ENTITY_SPEED = {
+        'Level1Bg0': 0,
+        'Level1Bg1': 1,
+        'Level1Bg2': 2,
+        'Level1Bg3': 3,
+        'Level1Bg4': 4,
+        'Level1Bg5': 5,
+        'Level1Bg6': 6,
+}
+
 
 # M
-MENU_OPTION = ('NEW GAME 1P'
-               'NEW GAME 2P - COOPERATIVE'
-               'NEW GAME 2P - COMPETITIVE'
-               'SCORE'
-               'EXIT')
+MENU_OPTION = (
+                'NEW GAME 1P',
+                'NEW GAME 2P - COOPERATIVE',
+                'NEW GAME 2P - COMPETITIVE',
+                'SCORE',
+                'EXIT'
+)
 
 
 # W
--- code/menu.py
+++ code/menu.py
@@ -21,8 +21,8 @@ class Menu:
         while True:
             # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_WHITE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", C_WHITE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
@@ -30,6 +30,7 @@ class Menu:
                 else:
                     self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()
+
 
             # Check for all events
             for event in pygame.event.get():
@@ -49,6 +50,8 @@ class Menu:
                             menu_option = len(MENU_OPTION) - 1
                     if event.key == pygame.K_RETURN:  # ENTER
                         return MENU_OPTION[menu_option]
+
+
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
